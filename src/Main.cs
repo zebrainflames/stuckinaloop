6f10cc55aaa63cf52e9eaa71afed28c3716cd348
jhkroger <jhkroger2@gmail.com>
2020-10-03 11:01:18 +0300

    Initial commit

---
+++ src/Main.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+namespace stuckinaloop
+{
+    public class Main : Node2D
+    {
+        
+        
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            GD.Print("Starting game..");    
+        }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+    }
+}
